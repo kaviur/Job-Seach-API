03f8145bbe4fe270b513a258a72c4a2fd1f3a037
jopareja <jonathan.pareja@juegaperu.com>
2022-04-27 21:14:29 -0500

    adding mongodb configuration

---
+++ src/index.js
@@ -0,0 +1,10 @@
+const express = require("express")
+const { port } = require("./config")
+const {connection} = require("./config/db")
+
+connection()
+const app = express()
+
+app.listen(port, ()=>{
+    console.log("Listening: http//localhost:"+port)
+})
