a20262fa96cebee79d48dc53be1249cb5fa5eadb
jopareja <jonathan.pareja@juegaperu.com>
2022-05-13 07:57:55 -0500

    filters

--- models/offer.js
+++ models/offer.js
@@ -3,12 +3,13 @@ const {mongoose} = require('../config/db')
 const offerSchema = new mongoose.Schema({
     title:String,
     description:String,
-    date: Date,
+    publish_date: Date,
     status:Number,
     programming_languages:[String],
     categories:[String],
     countries:[String],
     salary:Number,
+    mode:String,
     english_level:String,
     applicants:[String]
 })
